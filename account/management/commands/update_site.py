4cca52facb20c6cc2b158a0163edc096bc4eb76c
unknown <Mountain-Michaelle>
2024-04-25 21:46:30 +0100

    1. One submit

---
+++ account/management/commands/update_site.py
@@ -0,0 +1,12 @@
+from django.core.management.base import BaseCommand
+from django.contrib.sites.models import Site
+
+class Command(BaseCommand):
+    help = 'Update the site domain and name'
+
+    def handle(self, *args, **kwargs):
+        site = Site.objects.get_current()
+        site.domain = "https://article-blog-0wfl.onrender.com" # Use your React app URL here
+        site.name = 'Simple App'  # Choose a name for your site
+        site.save()
+        self.stdout.write(self.style.SUCCESS('Site domain and name updated successfully'))
