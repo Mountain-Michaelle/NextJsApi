0e1039f7fa339dd01644402cf6babd32a0e74cdd
unknown <Mountain-Michaelle>
2024-04-26 09:20:41 +0100

    about to host

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import UserAccount
 # Register your models here.
+
+
+class UserAccountAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'email')
+    list_display_links = ('id', 'email', )
+    search_fields = ('email',)
+    
+
+admin.site.register(UserAccount, UserAccountAdmin)
